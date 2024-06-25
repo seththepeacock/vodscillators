56484dbe0dc5132cbe68ac0f3d10011b8d9337a8
seththepeacock <seththepeacock@gmail.com>
2024-06-25 18:41:08 -0400

    Main implementation

--- main.py
+++ main.py
@@ -1,31 +1,62 @@
 from vodscillators import *
 import matplotlib.pyplot as plt
 import timeit
-from scipy.integrate import solve_ivp
+
 from numpy.fft import rfft, rfftfreq
 from statistics import mean
 
 
 start = timeit.default_timer() # starts timer that tells you code run time
 
-params = {"name" : "Frank",
-          "IC_method" : "rand",
-          "freq_dist" : "exp",
-          "loc_noise_amp" : 0.1,
-          "glob_noise_amp" : 0.1,
-          "epsilon" : 1.0,
-          "omega_o" : 2*np.pi,
-          "omega_n" : 5*np.pi,
-          "d_R" : 0.15,
-          "d_I" : -1.0,
-          "B" : 1.0,
-          "num_osc" : 100,
-          "n_transient" : 35855,
-          "n_ss" : 8192,
-          "num_runs" : 1, 
-          }
-v = Vodscillator(**params)
-print(v)
+p = {
+#General Initializing Params
+"name" : "Frank",
+"num_osc" : 100, # number of oscillators in chain[default = 100 or 150], 80 in paper
+
+#set_freq
+"freq_dist" : "exp", #linear or exp
+"roughness_amp" : 1,
+"omega_0" : 2*np.pi, # char frequency of lowest oscillator [default = 2*np.pi] 
+"omega_N" : 10*np.pi, # char frequency of highest oscillator [default = 5*(2*np.pi)] 
+
+#set_ICs
+"IC_method" : "rand", #rand or const
+
+#gen_noise
+"loc_noise_amp" : 0.1, #amplitude (sigma value) for local noise [0 --> off, default = 0.1-5]
+"glob_noise_amp" : 0.1, #amplitude (sigma value) for global noise [0 --> off, default = 0.1-5]
+"ti" : 0, # start time; [default = 0]
+"n_transient" : 35855, # the # of time points we give for transient behavior to settle down; around 30000 [default = 35855]
+"n_ss" : 8192, # the # of time points we observe the steady state behavior for [default = 8192]
+"num_runs" : 1, # [default for no noise is 1; when we have noise we average over multiple runs]
+"sample_rate" : 128, #[default = 128]
+
+#set_ODE
+"epsilon" : 1.0, # [default = 1.0] --> control parameter
+"d_R" : 0.15, # [default = 0.15] --> real part of coupling coefficient
+"d_I" : -1.0, # [default = -1.0] --> imaginary part of coupling coefficient
+"B" : 1.0, # [default = 1.0] --> amount of cubic nonlinearity
+}
+
+v = Vodscillator(**p)
+v.set_freq(**p)
+v.set_ICs(**p)
+v.gen_noise(**p)
+v.set_ODE(**p)
+v.solve_ODE(**p)
+v.save("frank.pkl")
+
+x = v.tpoints
+y = v.sol[0]
+
+plt.plot(x, y)
+plt.show()
+
+
+
+
+
+
 
     
 
--- vodscillators.py
+++ vodscillators.py
@@ -1,61 +1,178 @@
 import numpy as np
-from scipy import interpolate
-
+import pickle
+from scipy.interpolate import CubicSpline
+from scipy.integrate import solve_ivp
 
 class Vodscillator:
+  """
+  Vod-structions
+  1. Initialize
+  2. Create Frequency Distribution with "set_freq"
+  3. Set ICs with "set_ICs" 
+    - requires #2 first!
+  4. Generate Noise with "gen_noise"
+  5. Set parameters in ODE with "set_ODE"
+  6. Solve ODE Function with "solve_ODE"
+  7. Save your vodscillator to a file
+  7. Plot
+  """
+
+  #Note - you must create a frequency distribution before setting ICs since an oscillator's IC depends on its char freq
+
 
   def __init__(s, **p):
     # s = self (refers to the object itself)
     # **p unpacks the dictionary of parameters (p) we pass into the initializer
-    
-    s.name = p["name"] #name your vodscillator!
 
-    #set parameters
-
-    s.IC_method = p["IC_method"] # set to "rand" for randomized initial conditions, set to "const" for constant initial conditions
-    s.freq_dist = p["freq_dist"] # set to "linear" for linear frequency distribution, set to "exp" for exponential frequency distribution
-    s.loc_noise_amp = p["loc_noise_amp"] #amplitude (sigma value) for local noise [0 --> off, default = 0.1-5]
-    s.glob_noise_amp = p["glob_noise_amp"] #amplitude (sigma value) for global noise [0 --> off, default = 0.1-5]
-    s.epsilon = p["epsilon"] # [default = 1.0] --> damping
-    s.omega_0 = p["omega_0"]  # char frequency of loomegaest oscillator [default = 2*np.pi] 
-    s.omega_n = p["omega_n"]  # char frequency of highest oscillator [default = 5*(2*np.pi)] 
-    s.d_R = p["d_R"]  # [default = 0.15]
-    s.d_I = p["d_I"]  # [default = -1.0] 
-    s.B = p["B"] # [default = 1.0]
-    s.num_osc = p["num_osc"]  # number of oscillators in chain[default = 100 or 150], 80 in paper but didn't work when Olha tried --> number of oscillators
-    s.n_transient = p["n_transient"]  # the # of time points we give for transient behavior to settle down; around 30000 [default = 35855]
-    s.n_ss = p["n_ss"]  # the # of time points we observe the steady state behavior for [default = 8192]
-    s.num_runs = p["num_runs"] # [default for no noise is 1; when we have noise we average over multiple runs]
+    # GENERAL PARAMETERS
+    s.num_osc = p["num_osc"]  # number of oscillators in chain [default = 100 or 150]
     
-    #calculate the rest
-    
-    s.sample_rate = 128  #[default = 128] 
-    s.h = 1/s.sample_rate
-    s.ti = 0 # start time, [default = 0]
-    s.tf = s.h*s.n_transient + s.h*(s.n_ss + 1)  # end time
-    s.tpoints = np.arange(s.ti, s.tf, s.h) # array of time points
-    s.N = len(s.tpoints) #total num of points
-    
-    #get initial conditions
+    if "name" in p:
+      s.name = p["name"] # name your vodscillator!
 
-    #start with the frequency distribution array (omega)
-    #IMPLEMENT ROUGHNESS
 
+  def set_freq(s, **p):
+    # Freq Dist - creates s.omegas[]
+
+    # NECESSARY PARAMETERS
+    s.freq_dist = p["freq_dist"] # set to "linear" for frequency increasing linearly, set to "exp" for frequency increasing exponentially
+    s.roughness_amp = p["roughness_amp"] # variation in each oscillator's characteristic frequency
+    s.omega_0 = p["omega_0"]  # char frequency of lowest oscillator [default = 2*np.pi] 
+    s.omega_N = p["omega_N"]  # char frequency of highest oscillator [default = 5*(2*np.pi)] 
+
+    # Now we set the frequencies of each oscillator in our chain - linear or exponential
     if s.freq_dist == "linear":
-      s.omega = np.linspace(s.omega_0,s.omega_n,s.num_osc)
-    if s.freq_dist == "exp":
-      s.omega = np.zeros(s.num_osc, dtype=float)
-      for index in np.arange(0, s.num_osc):
-        s.omega[index] = s.omega_0*(s.omega_n/s.omega_0)**(index / (s.num_osc - 1))
-        #CHECK THIS IS CORRECT
+      s.omegas = np.linspace(s.omega_0,s.omega_N,s.num_osc) # linearly spaced frequencies from omega_0 to omega_N
+      # add roughness
+    elif s.freq_dist == "exp":
+      s.omegas = np.zeros(s.num_osc, dtype=float)
+      for k in range(s.num_osc): # exponentially spaced frequencies from omega_0 to omega_N
+        A = s.omega_0
+        B = (s.omega_N/s.omega_0)**(1/(s.num_osc - 1))
+        s.omegas[k] = A*(B**k)
+        # note that omegas[0] = A = omega_0, and omegas[num_osc - 1] = A * B = omega_N
 
+    # add roughness
+    r = s.roughness_amp
+    roughness = np.random.uniform(low=-r, high=r, size=(s.num_osc,))
+    s.omegas = s.omegas + roughness
 
+  def set_ICs(s, **p):
+    # Generates s.ICs[]
+    # Note: we need omegas[] for this, so set_freq first!  
+
+    # NECESSARY PARAMETER
+    s.IC_method = p["IC_method"]  # set to "rand" for randomized initial conditions, set to "const" for constant initial conditions
+    
+    s.ICs = np.zeros(s.num_osc, dtype=complex)
 
     if s.IC_method == "rand":
-      s.omega = 3
+      # generate random ICs
+      for k in range(s.num_osc):
+        x_k = np.random.uniform(-1, 1)
+        y_k = np.random.uniform(-1, 1) # this was (0,1) in Beth's Code, not sure why yet
+        s.ICs[k] = complex(x_k, y_k) # make a complex combination of x and y and save it in ICs
+    elif s.IC_method == "const":
+      # generate predetermined ICs
+      all_x = np.linspace(-1, 1, s.num_osc)
+      all_y = np.linspace(1, -1, s.num_osc)
+      for k in range(s.num_osc):
+        x_k = all_x[k]
+        y_k = all_y[k]
+        s.ICs[k] = x_k - (y_k*1j/s.omegas[k])
+
+  def gen_noise(s, **p):
+    # Generating Noise - creates s.xi_glob and s.xi_loc[]
+
+    # NECESSARY PARAMETERS
+    s.loc_noise_amp = p["loc_noise_amp"] # amplitude (sigma value) for local noise [0 --> off, default = 0.1-5]
+    s.glob_noise_amp = p["glob_noise_amp"] # amplitude (sigma value) for global noise [0 --> off, default = 0.1-5]
+    s.ti = p["ti"] # start time; [default = 0]
+    s.n_transient = p["n_transient"]  # the # of time points we give for transient behavior to settle down; around 30000 [default = 35855]
+    s.n_ss = p["n_ss"]  # the # of time points we observe the steady state behavior for [default = 8192]
+    s.num_runs = p["num_runs"] # [default for no noise is 1; with noise we must average over multiple runs]
+    s.sample_rate = p["sample_rate"] # [default = 128]
+
+    # Calculate other params
+    s.h = 1/s.sample_rate #delta t between time points
+    s.tf = s.h*s.n_transient + s.h*(s.n_ss + 1)  # end time - not sure what this + 1 is for
+
+    # We want a global xi(t) and then one for each oscillator. 
+
+    # First, generate time points with a little padding at the end to smooth the interpolation
+    s.padded_tpoints = np.arange(s.ti, s.tf + 2*s.h, s.h)
+
+    # global --> will impact each oscillator equally at each point in time (e.g., wind blowing?)
+    # first we randomly generate points with a standard normal distribution
+    global_noise = s.glob_noise_amp * np.random.randn(len(s.padded_tpoints)) # normal distribution times noise amplitude
+    # then interpolate between (using a cubic spline) for ODE solving adaptive step purposes
+    s.xi_glob = CubicSpline(s.padded_tpoints, global_noise)
+
+    # local --> will impact each oscillator differently at each point in time (e.g., brownian motion of fluid in inner ear surrounding hair cells)
+    s.xi_loc = np.empty(s.num_osc, dtype=CubicSpline)
+    for k in range(s.num_osc):
+      # again, we randomly generate points (standarad normal) then interpolate between
+      local_noise = s.loc_noise_amp * np.random.randn(len(s.padded_tpoints))
+      s.xi_loc[k] = CubicSpline(s.padded_tpoints, local_noise)
+
+
+  def set_ODE(s, **p):
+    # Setting Parameters for Final Function 
+
+    # NECESSARY PARAMETERS
+    s.epsilon = p["epsilon"] # [default = 1.0] --> control parameter
+    s.d_R = p["d_R"]  # [default = 0.15] --> real part of coupling coefficient
+    s.d_I = p["d_I"]  # [default = -1.0] --> imaginary part of coupling coefficient
+    s.B = p["B"] # [default = 1.0] --> amount of cubic nonlinearity
+
+  def solve_ODE(s, **p):
+    s.tpoints = np.arange(s.ti, s.tf, s.h) # array of time points
+    s.sol = solve_ivp(s.ODE, [s.ti, s.tf], s.ICs, t_eval=s.tpoints).y 
+    # adding ".y" grabs the solutions - an array of arrays, where each y value corresponds to t points.
+    # so s.sol[2][4] is the value of the solution for the 3rd oscillator at the 5th time point.
     
+
+  def save(s, filename):
+    # filename must be a string like "file.pkl"
+    with open(filename, 'wb') as outp:  # Overwrites any existing file.
+        pickle.dump(s, outp, pickle.HIGHEST_PROTOCOL)
+
+  def open(s, filename):
+    # filename must be a string like "file.pkl"
+    # this will overwrite the current object
+    with open(filename, 'rb') as picklefile:
+      s = pickle.load(picklefile)
+
   def __str__(s):
     return f"A vodscillator named {s.name} with {s.num_osc} oscillators!"
+  
+  def ODE(s, t, z):
+    # This function will only be called by the ODE solver
+
+    # Note that this is equation (11) in V&D 2008
+
+    # First make an array to represent the current (complex) derivative of each oscillator
+    ddt = np.zeros(s.num_osc, dtype=complex)
+
+    # Define the complex coupling constant (ccc)
+    ccc =  s.d_R + 1j*s.d_I
+
+    for k in range(0, s.num_osc - 1):
+      # This part of the equation is the same for all oscillators. 
+      # (Note our xi are functions of time, and z[k] is the current position of the k-th oscillator)
+      universal = (1j*s.omegas[k]* + s.epsilon)*z[k] + s.xi_glob(t) + s.xi_loc[k](t) - s.B*((np.abs(z[k]))**2)*z[k]
+
+      # if we're in the middle of the chain, we couple with the oscillator on either side
+      if k != 0 & k != (s.num_osc - 1):
+        ddt[k] = universal + ccc*((z[k+1] - z[k]) + (z[k-1] - z[k]))
+
+      # But if we're at an endpoint, we only have one oscillator to couple with
+      elif k == 0:
+        ddt[k] = universal + ccc*(z[k+1] - z[k])
+      elif k == s.num_osc - 1:
+        ddt[k] = universal + ccc*(z[k-1] - z[k])
+      
+    return ddt
 
 
     
