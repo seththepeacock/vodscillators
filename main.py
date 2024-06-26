b0fb55b2e5500b1b4ce20ec86942f87471c33a9d
seththepeacock <seththepeacock@gmail.com>
2024-06-26 17:56:31 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,19 @@ import matplotlib.pyplot as plt
 import timeit
 import pickle
 
+
+#WELCOME TO SETH's BRANCH
+
+
+# import json
+# with open('output.txt', 'w') as filehandle:
+#     json.dump(v.t.tolist(), filehandle)
+
+# plt.plot(v.t, v.summed_sol.real, v.t, v.summed_sol.imag)
+# plt.savefig("summed_response")
+# plt.show()
+
+
 with open("frank.pkl", 'rb') as picklefile:
     v = pickle.load(picklefile)
 # Now we can use our vodscillator v with its solution pre-solved for!
