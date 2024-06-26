230d5d6115dee9c68f8f252f2976f19eae9469bf
seththepeacock <seththepeacock@gmail.com>
2024-06-26 14:23:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,8 @@ import timeit
 import pickle
 
 # import json
-
 # with open('output.txt', 'w') as filehandle:
 #     json.dump(v.t.tolist(), filehandle)
-
 
 # plt.plot(v.t, v.summed_sol.real, v.t, v.summed_sol.imag)
 # plt.savefig("summed_response")
@@ -18,6 +16,10 @@ with open("frank.pkl", 'rb') as picklefile:
     v = pickle.load(picklefile)
 
 # Now we can use our vodscillator v with its solution pre-solved for!
+# We can get the solution (complex output as a function of time) with v.sol[index] where "index" is the index of the oscillator. 
+# If we want the summed solution (all of the oscillators summed together) we grab v.summed_sol 
+
+
 
 # Generating V&D figs 2-5 style clustering graphs
 
