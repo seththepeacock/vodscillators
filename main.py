bccac601f5ba7ab0cd24156056c1ade99cc713f9
seththepeacock <seththepeacock@gmail.com>
2024-07-02 17:43:01 -0400

    filename

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import timeit
 import pickle
 
 # Open pickled vodscillator
-filename = "Frank.pkl"
+filename = "V&D fig 2A NO NOISE.pkl"
 with open(filename, 'rb') as picklefile:
     v = pickle.load(picklefile)
     # this "assert" statement will let VSCode know that this is a Vodscillator, so it will display its documentation for you!
