1400baf2cade945bf99690fe7a14f5133f50adf6
denizunel <idunel02@gmail.com>
2024-07-02 18:42:37 -0400

    ah

--- main.py
+++ main.py
@@ -5,19 +5,13 @@ import timeit
 import pickle
 
 # Open pickled vodscillator
-filename = "nonoise.pkl"
+filename = "vd-no-noise.pkl"
 with open(filename, 'rb') as picklefile:
     v = pickle.load(picklefile)
     # this "assert" statement will let VSCode know that this is a Vodscillator, so it will display its documentation for you!
     assert isinstance(v, Vodscillator)
 
-start = timeit.default_timer()
 
-#v.coherence()
-#v.save()
-
-stop = timeit.default_timer() 
-print('Total time:', stop - start, "seconds, or", (stop-start)/60, "minutes") 
 
 plt.plot(v.fft_freq, v.SOO_phase_coherence)
 plt.show()
