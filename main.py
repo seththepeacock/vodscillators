5859d03f53be5c8a983d757efd765d87dc042341
denizunel <idunel02@gmail.com>
2024-07-04 15:57:21 -0400

    pleaaaaaaase

--- main.py
+++ main.py
@@ -18,25 +18,7 @@ v.plotter("superimpose")
 
 
 
-def c_psd_logplot(v): #logarithmic coherence and psd plot
-    v.psd()
-    v.coherence()
-    plt.plot(v.fft_freq, np.log(v.psd), label="psd")
-    plt.plot(v.fft_freq, v.SOO_phase_coherence*10, label="coherence")
-    plt.legend()
-
-def phase_portrait(v):
-    xdot = np.imag((v.sol))
-    x = np.real((v.sol))
-    plt.plot(x, xdot)
-    plt.grid()
-
-
-#write stuff like this!!!
-#vlodder.coherence(v.SOO_fft)
-
-def heat_map(v):
-    pass
+    
 
 
 plt.show()
