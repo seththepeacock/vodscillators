1a81244b1208da8d6ef6762c0868581099ee2055
denizunel <idunel02@gmail.com>
2024-07-04 15:11:10 -0400

    eyeyey

--- main.py
+++ main.py
@@ -18,27 +18,23 @@ with open(filename, 'rb') as picklefile:
 #v.plot_waveform()
 
 
+def c_psd_logplot(v): #logarithmic coherence and psd plot
+    v.psd()
+    v.coherence()
+    plt.plot(v.fft_freq, np.log(v.psd), label="psd")
+    plt.plot(v.fft_freq, v.SOO_phase_coherence*10, label="coherence")
+    plt.legend()
 
-"""
-logaritmic plot for coherence & psd
-------------------------
-v.psd()
-v.coherence()
-plt.plot(v.fft_freq, np.log(v.psd), label="psd")
-plt.plot(v.fft_freq, v.SOO_phase_coherence*10, label="coherence")
-plt.legend()
-"""
+def phase_portrait(v):
+    xdot = np.imag((v.sol))
+    x = np.real((v.sol))
+    plt.plot(x, xdot)
+    plt.grid()
 
-xdot = np.imag((v.sol))
-
-x = np.real((v.sol))
-
-plt.plot(x, xdot)
-
-plt.grid()
 
 #write stuff like this!!!
 #vlodder.coherence(v.SOO_fft)
+
 
 plt.show()
 
