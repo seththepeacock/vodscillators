901fe34acddd72a2af4f2aa72742f7001adbb4ed
denizunel <idunel02@gmail.com>
2024-07-05 13:09:37 -0400

    a

--- deniz_main.py
+++ deniz_main.py
@@ -5,9 +5,10 @@ import timeit
 import pickle
 from plots import *
 import seaborn as sns
+import scipy
 
 
-if 1==1:
+if 0==1:
     # Open pickled vodscillator
     with open("vd-no-noise.pkl", 'rb') as picklefile:
         v = pickle.load(picklefile)
@@ -21,11 +22,9 @@ if 0==1:
         # this "assert" statement will let VSCode know that this is a Vodscillator, so it will display its documentation for you!
         assert isinstance(v, Vodscillator)
 
-#v.plotter("cluster")
-#v.plotter("PSD")
-#v.plotter("superimpose")
-#phase_portrait(v)
 
-heat_map(v)
+
+
+#heat_map(v)
 plt.show()
 
