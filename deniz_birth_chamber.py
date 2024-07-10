ea304b0f9e98ba50f56b32dd8a2b86574c1f64f1
denizunel <idunel02@gmail.com>
2024-07-10 14:33:51 -0400

    a

--- deniz_birth_chamber.py
+++ deniz_birth_chamber.py
@@ -11,13 +11,13 @@ start = timeit.default_timer() # starts timer that tells you code runtime
 p = {
 #General Initializing Params
 "name" : "newpickle",
-"num_osc" : 80, # number of oscillators in chain[default = 100 or 150], 80 in paper
+"num_osc" : 50, # number of oscillators in chain[default = 100 or 150], 80 in paper
 
 #set_freq
-"freq_dist" : "linear", #linear or exp
+"freq_dist" : "exp", #linear or exp
 "roughness_amp" : 0,
-"omega_0" : 2*np.pi, # char frequency of lowest oscillator [default = 2*np.pi] 
-"omega_N" : 5*2*np.pi, # char frequency of highest oscillator [default = 5*(2*np.pi)] 
+"omega_0" : 5, # char frequency of lowest oscillator [default = 2*np.pi] 
+"omega_N" : 25, # char frequency of highest oscillator [default = 5*(2*np.pi)] 
 
 #set_ICs
 "IC_method" : "rand", #rand or const
@@ -29,14 +29,14 @@ p = {
 "t_transient" : 280, # how long we give for transient behavior to settle down [default = 280 --> n.transient = 35840]
 "t_ss" : 64, # length of an interval of ss observation [default = 64 --> n.transient = 8192]
 "num_intervals" : 30, # [default for no noise is 1; when we have noise we average over multiple intervals, default = 30]
-"sample_rate" : 1024, #[default = 128]
+"sample_rate" : 512, #[default = 128]
 
 #set_ODE"
 "epsilon" : 1.0, # [default = 1.0] --> control parameter
 "d_R" : 2.0, # [default = 0.15] --> real part of coupling coefficient
 "d_I" : -2.0, # [default = -1.0] --> imaginary part of coupling coefficient
 "alpha" : 1.0, # [default = 1.0] --> real coefficient for cubic nonlinearity
-"beta_sigma" : 0.0 # [0 = isochronous as in V&D] --> std dev (normal dist w/ 0 mean) for beta_j... 
+"beta_sigma" : 2.0 # [0 = isochronous as in V&D] --> std dev (normal dist w/ 0 mean) for beta_j... 
 # imaginary coefficient for cubic nonlinearity (beta_j) which creates nonisochronicity
 }
 
@@ -44,7 +44,9 @@ v = Vodscillator(**p)
 v.initialize(**p)
 v.gen_noise(**p)
 v.solve_ODE()
+print("Doing FFT")
 v.do_fft()
+print("Saving")
 v.save()
 
 stop = timeit.default_timer() # ends timer
