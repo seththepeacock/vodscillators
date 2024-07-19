f534c73780d7b501d977720c41e76d8871c1468a
denizunel <idunel02@gmail.com>
2024-07-19 16:06:48 -0400

    a

--- plots.py
+++ plots.py
@@ -345,7 +345,7 @@ def coherence_vs_psd(wf, sample_rate, t_win, t_shift=None, num_wins=None, khz=Fa
   if ax is None:
     plt.figure(fig_num)
     ax = plt.gca()
-  #assert isinstance(ax, Axes)
+  assert isinstance(ax, Axes)
   
   # now we'll add an axes object with identical x-axis and flexible y-axis (which we'll add the psd to)
   ax2 = ax.twinx()
