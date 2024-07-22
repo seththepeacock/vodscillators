f74a6477f29c3402bbc554fd860862f8236c359c
denizunel <idunel02@gmail.com>
2024-07-22 15:08:16 -0400

    a

--- deniz_misc2.py
+++ deniz_misc2.py
@@ -5,32 +5,30 @@ import pickle
 from plots import *
 from vlodder import *
 import scipy.io
-from scipy.signal import welch
-#from scipy.signal import *
+from scipy.signal import *
 
-#filename = "V&D fig 2A, loc=0.1, glob=0.1.pkl"
-#filepath = "/home/deniz/Dropbox/vodscillators/deniz pickle jar/"
+filename = "F&B fig 2D noniso.pkl"
+filepath = "/home/deniz/Dropbox/vodscillators/deniz pickle jar/"
+with open(filepath + filename, 'rb') as picklefile:
+    vod = pickle.load(picklefile)
+    #this "assert" statement will let VSCode know that this is a Vodscillator, so it will display its documentation for you!
+    assert isinstance(vod, Vodscillator)
 
+vod.n_win = vod.n_ss
+vod.save()
 
-#with open(filepath + filename, 'rb') as picklefile:
-#    vod = pickle.load(picklefile)
-#    # this "assert" statement will let VSCode know that this is a Vodscillator, so it will display its documentation for you!
-#    assert isinstance(vod, Vodscillator)
-
-#vod.n_win = vod.n_ss
-
-# vod.save()
-
-
-filename = 'AC6rearSOAEwfB1.mat'
-mat = scipy.io.loadmat('SOAE Data/' + 'AC6rearSOAEwfB1.mat')
-wf = np.squeeze(mat['wf'])
+#filename = 'AC6rearSOAEwfB1.mat'
+#filename = 'TH14RearwaveformSOAE.mat'
+#mat = scipy.io.loadmat('SOAE Data/' + filename)
+#wf = np.squeeze(mat['wf'])
 wf_title = filename
+
+wf = vod.SOO_sol
 
 
 sample_rate=44100
 xmin=None
-xmax=None
+xmax=5
 ymin=None
 ymax=None
 # ymin=0
@@ -40,7 +38,12 @@ t_win = 0.1
 hann = True
 t_shift = 0.1
 
-ax1 = plt.subplot(2, 2, 1)
+
+fig, (ax1, ax2) = plt.subplots(2) #no hann
+figg, (ax3, ax4) = plt.subplots(2) #yes hann
+fig.suptitle('No Hann')
+figg.suptitle("Yes Hann")
+
 ref_type = "next_win"
 coherence_vs_psd(ax=ax1, t_shift=t_shift, wf_title=wf_title, wf=wf, ref_type=ref_type, t_win=t_win, sample_rate=sample_rate,
                  xmin=xmin, xmax=xmax, khz=True, show_plot=show_plot, hann=False)
@@ -48,31 +51,25 @@ ax1.legend(loc='upper right')
 ax1.set_title("Next window ")
 
 
-
-ax2 = plt.subplot(2, 2, 2)
-ref_type = "next_win"
-coherence_vs_psd(ax=ax2, t_shift=t_shift, wf_title=wf_title, wf=wf, ref_type=ref_type, t_win=t_win, sample_rate=sample_rate,
-                 xmin=xmin, xmax=xmax, khz=True, show_plot=show_plot, hann=hann)
-ax2.legend(loc='upper right')
-ax2.set_title("Next window Hann")
-
-ax3 = plt.subplot(2, 2, 3)
 ref_type="next_freq"
-coherence_vs_psd(wf_title=wf_title, t_shift=t_shift, ax=ax3, wf=wf, ref_type=ref_type, t_win=t_win, sample_rate=sample_rate,
+coherence_vs_psd(wf_title=wf_title, t_shift=t_shift, ax=ax2, wf=wf, ref_type=ref_type, t_win=t_win, sample_rate=sample_rate,
                  xmin=xmin, xmax=xmax, khz=True, show_plot=show_plot, hann=False)
+ax2.legend(loc='upper right')
+ax2.set_title("Next frequency ")
+
+
+ref_type = "next_win"
+coherence_vs_psd(ax=ax3, t_shift=t_shift, wf_title=wf_title, wf=wf, ref_type=ref_type, t_win=t_win, sample_rate=sample_rate,
+                 xmin=xmin, xmax=xmax, khz=True, show_plot=show_plot, hann=hann)
 ax3.legend(loc='upper right')
-ax3.set_title("Next frequency ")
+ax3.set_title("Next window")
 
 
-
-ax4 = plt.subplot(2, 2, 4)
 ref_type="next_freq"
 coherence_vs_psd(wf_title=wf_title, t_shift=t_shift, ax=ax4, wf=wf, ref_type=ref_type, t_win=t_win, sample_rate=sample_rate,
                  xmin=xmin, xmax=xmax, khz=True, show_plot=show_plot, hann=hann)
 ax4.legend(loc='upper right')
-ax4.set_title("Next frequency Hann")
-
-
+ax4.set_title("Next frequency")
 
 
 plt.tight_layout()
