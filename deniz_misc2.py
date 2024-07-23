bd5b2751ca89f485f1da41621601b150e4c9f117
denizunel <idunel02@gmail.com>
2024-07-22 21:29:28 -0400

    a

--- deniz_misc2.py
+++ deniz_misc2.py
@@ -7,36 +7,38 @@ from vlodder import *
 import scipy.io
 from scipy.signal import *
 
-filename = "F&B fig 2D noniso.pkl"
-filepath = "/home/deniz/Dropbox/vodscillators/deniz pickle jar/"
-with open(filepath + filename, 'rb') as picklefile:
-    vod = pickle.load(picklefile)
-    #this "assert" statement will let VSCode know that this is a Vodscillator, so it will display its documentation for you!
-    assert isinstance(vod, Vodscillator)
-
-vod.n_win = vod.n_ss
-vod.save()
+#filename = "F&B fig 2D iso.pkl"
+##filename = "F&B fig 2D noniso.pkl"
+#filepath = "/home/deniz/Dropbox/vodscillators/deniz pickle jar/"
+#with open(filepath + filename, 'rb') as picklefile:
+#    vod = pickle.load(picklefile)
+#    #this "assert" statement will let VSCode know that this is a Vodscillator, so it will display its documentation for you!
+#    assert isinstance(vod, Vodscillator)
+#
+##vod.n_win = vod.n_ss
+##vod.save()
 
 #filename = 'AC6rearSOAEwfB1.mat'
-#filename = 'TH14RearwaveformSOAE.mat'
-#mat = scipy.io.loadmat('SOAE Data/' + filename)
-#wf = np.squeeze(mat['wf'])
+filename = 'TH14RearwaveformSOAE.mat'
+mat = scipy.io.loadmat('SOAE Data/' + filename)
+wf = np.squeeze(mat['wf'])
 wf_title = filename
 
-wf = vod.SOO_sol
+#wf = vod.SOO_sol
 
 
 sample_rate=44100
-xmin=None
+win_size = 4096
+xmin=0
 xmax=5
 ymin=None
 ymax=None
 # ymin=0
 # ymax=8
 show_plot=False
-t_win = 0.1
+t_win = win_size / sample_rate
 hann = True
-t_shift = 0.1
+t_shift = t_win / 2 #set this to half the window size #it used to be 0.1
 
 
 fig, (ax1, ax2) = plt.subplots(2) #no hann
@@ -72,5 +74,4 @@ ax4.legend(loc='upper right')
 ax4.set_title("Next frequency")
 
 
-plt.tight_layout()
 plt.show()
